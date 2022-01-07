99b908b482b1859dfbdd7bec132019e12425fd68
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-01-07 22:46:43 +0800

    Update mosdns-cn-install-arm64.sh

--- mosdns-cn-install-arm64.sh
+++ mosdns-cn-install-arm64.sh
@@ -6,7 +6,7 @@ cd /opt/mosdns-cn
 wget https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-arm64.zip
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-wget https://github.com/allanchen2019/mosdns-cn-debian-install/blob/main/my-config.yaml
+wget https://raw.githubusercontent.com/allanchen2019/mosdns-cn-debian-install/main/my-config.yaml
 unzip mosdns-cn-linux-arm64.zip
 ./mosdns-cn --service install --config my-config.yaml
 ./mosdns-cn --service start
