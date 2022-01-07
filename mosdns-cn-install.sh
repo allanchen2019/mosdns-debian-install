f9d1c334dc9300768fb09a6fd89598ef266fbdd8
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-01-08 01:35:28 +0800

    Update mosdns-cn-install.sh

--- mosdns-cn-install.sh
+++ mosdns-cn-install.sh
@@ -6,10 +6,10 @@ architecture=$(dpkg --print-architecture)
 apt install -y unzip
 mkdir -p /opt/mosdns-cn
 cd /opt/mosdns-cn
-curl -fsSLo-  https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
-curl -fsSLo-  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-curl -fsSLo-  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-curl -fsSLo-  https://raw.githubusercontent.com/allanchen2019/mosdns-cn-debian-install/main/my-config.yaml
+curl -O https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
+curl -O https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+curl -O https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+curl -O https://raw.githubusercontent.com/allanchen2019/mosdns-cn-debian-install/main/my-config.yaml
 unzip mosdns-cn-linux-$architecture".zip
 ./mosdns-cn --service install --config my-config.yaml
 ./mosdns-cn --service start
