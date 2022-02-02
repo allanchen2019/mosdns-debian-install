f720c4d5bd6ad3d4c6c132dc7f30cea025b401e7
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-02-02 23:26:55 +0800

    Update mosdns-cn-install.sh

--- mosdns-cn-install.sh
+++ mosdns-cn-install.sh
@@ -6,12 +6,16 @@ architecture=$(dpkg --print-architecture)
 apt install -y unzip
 mkdir -p /opt/mosdns-cn
 cd /opt/mosdns-cn
+
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/my-config.yaml https://raw.githubusercontent.com/allanchen2019/mosdns-cn-debian-install/main/my-config.yaml
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/update-geo.sh https://github.com/allanchen2019/mosdns-cn-debian-install/blob/main/update-geo.sh
+chmod +x update-geo.sh
 unzip mosdns-cn.zip
 ./mosdns-cn --service install --config my-config.yaml
 ./mosdns-cn --service start
 systemctl status mosdns-cn.service
+
+/bin/bash -c 'echo "0 7 * * * root /opt/mosdns-cn/update-geo.sh" >> /etc/crontab'
