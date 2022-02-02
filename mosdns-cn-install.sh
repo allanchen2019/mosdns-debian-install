d3e2a3e19031cd74c252c79567880f8d476e83b4
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-02-02 23:04:06 +0800

    Update mosdns-cn-install.sh

--- mosdns-cn-install.sh
+++ mosdns-cn-install.sh
@@ -10,6 +10,7 @@ wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/mosdns-cn.zip https://github
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/my-config.yaml https://raw.githubusercontent.com/allanchen2019/mosdns-cn-debian-install/main/my-config.yaml
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/update-geo.sh https://github.com/allanchen2019/mosdns-cn-debian-install/blob/main/update-geo.sh
 unzip mosdns-cn.zip
 ./mosdns-cn --service install --config my-config.yaml
 ./mosdns-cn --service start
