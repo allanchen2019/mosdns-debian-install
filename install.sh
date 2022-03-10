27bcb83f500c6a505affc76529e8906d638f47bb
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-03-10 11:08:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,16 +9,17 @@ cd /opt
 git clone https://ghproxy.com/https://github.com/allanchen2019/mosdns-cn-debian-install.git
 mv mosdns-cn-debian-install mosdns-cn
 cd mosdns-cn
-
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/mosdns-cn.zip https://ghproxy.com/https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geoip.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-
 chmod +x *.sh
-cd ..
-unzip -do mosdns-cn.zip
-cd mosdns-cn
-./mosdns-cn --service install --config config.yaml
+mkdir -p bin
+cd bin
+
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/mosdns-cn.zip https://ghproxy.com/https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+
+unzip -o mosdns-cn.zip
+#cd mosdns-cn
+./mosdns-cn --service install --config ./..config.yaml
 ./mosdns-cn --service start
 systemctl status mosdns-cn.service
 
