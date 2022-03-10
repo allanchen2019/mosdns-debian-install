8f1880c8f693ff4dff26d95164dea6fd9838b345
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-03-10 11:39:46 +0800

    Update update-geo.sh

--- update-geo.sh
+++ update-geo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #update geofile
-cd /opt/mosdns-cn
-wget -O /opt/mosdns-cn/geoip.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget -O /opt/mosdns-cn/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+cd /opt/mosdns-cn/bin
+wget -O /opt/mosdns-cn/bin/geoip.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -O /opt/mosdns-cn/bin/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 ./mosdns-cn --service restart
