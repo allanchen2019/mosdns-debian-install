078584cf22d3778bfc838885c2732729995234cc
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-03-10 10:58:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geoip.dat https://ghproxy.co
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
 chmod +x *.sh
-unzip -o mosdns-cn.zip
+cd ..
+unzip -do mosdns-cn.zip
+cd mosdns-cn
 ./mosdns-cn --service install --config config.yaml
 ./mosdns-cn --service start
 systemctl status mosdns-cn.service
