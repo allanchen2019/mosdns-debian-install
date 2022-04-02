190faf4027c6b0e8670e2719497a889a6c5bd047
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-04-02 11:15:21 +0800

    Update update-bin.sh

--- update-bin.sh
+++ update-bin.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+set -xeuo pipefail
 clear
 architecture=$(dpkg --print-architecture)
-cd /opt/mosdns-cn/bin
+cd /opt/mosdns-cn/bin || exit
 rm mosdns-cn*
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/mosdns-cn.zip https://ghproxy.com/https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
 unzip -o mosdns-cn.zip
 ./mosdns-cn --service restart
 journalctl -xeu mosdns-cn.service
