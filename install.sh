9f201fb22a1c6ba706638a109ad193fd8fadcaa1
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-01 13:31:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,15 @@ cd bin || exit
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
+wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
+sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
+sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
 
 unzip -o mosdns-cn.zip
 #cd mosdns-cn
 ./mosdns-cn --service install --config /opt/mosdns-cn/config.yaml
 ./mosdns-cn --service start
-systemctl status mosdns-cn.service
-
 /bin/bash -c 'echo "0 12 * * * root /opt/mosdns-cn/update-geo.sh" >> /etc/crontab'
+journalctl -f
