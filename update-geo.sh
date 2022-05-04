9dca055d8567214778a92c5b419fdd139bd5009d
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-04 23:30:43 +0800

    Update update-geo.sh

--- update-geo.sh
+++ update-geo.sh
@@ -5,8 +5,8 @@ clear
 
 cd /opt/mosdns-cn/bin || exit
 
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -O geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -O geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
 wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
 wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
