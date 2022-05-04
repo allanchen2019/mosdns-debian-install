ea82f758fc84fcccc56a61244fecfd33d2b38bbd
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-04 23:32:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ set -xeuo pipefail
 clear
 architecture=$(dpkg --print-architecture)
 
-apt install -y unzip
+#apt install -y unzip
 cd /opt/mosdns-cn || exit
 mkdir bin
 cd bin || exit
 
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -O geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -O geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
 wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
 wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
