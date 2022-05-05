3382ac71d70dd120681bf4d4142d09e16ccf6ada
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-06 00:14:34 +0800

    Update update-geo.sh

--- update-geo.sh
+++ update-geo.sh
@@ -8,10 +8,16 @@ cd /opt/mosdns-cn/bin || exit
 wget -O geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget -O geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget -O apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
+wget -O google.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/google.china.conf
 wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
 wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
 sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
 sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
+sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
+sed -r 's/.{16}$//' apple.china.conf2 > apple.china.conf.raw.txt
+sed -r 's/.{8}//' google.china.conf > google.china.conf2
+sed -r 's/.{16}$//' google.china.conf2 > google.china.conf.raw.txt
 
 ./mosdns-cn --service restart
 journalctl -f
