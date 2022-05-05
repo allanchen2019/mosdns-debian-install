3a5fcdc16ca2fdf151abd88f6cb35e9b754ee5e5
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-05 23:08:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,24 @@ set -xeuo pipefail
 clear
 architecture=$(dpkg --print-architecture)
 
-#apt install -y unzip
 cd /opt/mosdns-cn || exit
 mkdir bin
 cd bin || exit
 
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
-wget -O geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget -O geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget -O apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
+wget -O google.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/google.china.conf
 wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
 wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
 sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
 sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
+sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
+sed -r 's/.{16}$//' apple.china.conf2 > apple.china.conf.raw.txt
+sed -r 's/.{8}//' google.china.conf > google.china.conf2
+sed -r 's/.{16}$//' google.china.conf2 > google.china.conf.raw.txt
 
 unzip -o mosdns-cn.zip
 #cd mosdns-cn
