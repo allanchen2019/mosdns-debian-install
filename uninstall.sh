701639fa992111c1d5dfcdf93c3cd88b050f3c9a
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-27 03:18:18 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,14 @@
 set -xeuo pipefail
 clear
 
+rm -rf /opt/mosdns-cn
+sed -i '/mosdns-cn/d' /etc/crontab
+
 systemctl stop mosdns-cn.service
 systemctl disable mosdns-cn.service
 rm /etc/systemd/system/mosdns-cn.service
 systemctl daemon-reload
 systemctl reset-failed
 
-rm -rf /opt/mosdns-cn
-sed -i '/mosdns-cn/d' /etc/crontab
+
 echo "Uninstall complete."
