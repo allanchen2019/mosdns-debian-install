f046246afd8f5ad77486c4c173e9d8c399be063a
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-27 03:14:56 +0800

    Update AutoSetup.sh

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "下载依赖……"
 apt update
 apt install wget git unzip -y
@@ -7,8 +8,6 @@ git clone https://github.com/allanchen2019/mosdns-cn-debian-install.git /opt/mos
 chmod 777 -R /opt/mosdns-cn
 echo "运行安装脚本……"
 bash /opt/mosdns-cn/install-mosdns.sh
-echo "--------------------"
-echo "完成！您已成功安装mosdns-cn！"
-echo "若您需要卸载，请使用“bash /opt/mosdns-cn/uninstall.sh”指令。"
-echo "--------------------"
 rm -rf ./AutoSetup.sh
+
+systemctl status mosdns-cn.service
