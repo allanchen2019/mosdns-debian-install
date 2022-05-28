e5849b80838c270a32afed5ac67ff04627ed57e1
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-28 21:13:27 +0800

    Update install-mosdns.sh

--- install-mosdns.sh
+++ install-mosdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xeuo pipefail
+set -xeuo
 clear
 architecture=$(dpkg --print-architecture)
 
@@ -10,11 +10,11 @@ cd bin || exit
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/mosdns-cn.zip https://github.com/IrineSistiana/mosdns-cn/releases/latest/download/mosdns-cn-linux-"$architecture".zip
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget --show-progress -t 5 -T 10 -cqO /opt/mosdns-cn/bin/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
-wget -O apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
-wget -O google.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/google.china.conf
-wget -O chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
-wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
+wget --show-progress -t 5 -T 10 -cqO accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget --show-progress -t 5 -T 10 -cqO apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
+wget --show-progress -t 5 -T 10 -cqO google.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/google.china.conf
+wget --show-progress -t 5 -T 10 -cqO chnroutes.txt https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt
+wget --show-progress -t 5 -T 10 -cqO anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
 sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
 sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
 sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
