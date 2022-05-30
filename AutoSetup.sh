dfe9573acb2a583dcb933a91ad0c1f1d18d0b76a
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-05-31 01:29:54 +0800

    Update AutoSetup.sh

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -4,7 +4,7 @@ clear
 
 echo "下载依赖……"
 apt update
-apt install wget git unzip -y
+apt install wget git unzip pip -y
 echo "克隆存储库……"
 git clone https://github.com/allanchen2019/mosdns-cn-debian-install.git /opt/mosdns-cn
 chmod 777 -R /opt/mosdns-cn
