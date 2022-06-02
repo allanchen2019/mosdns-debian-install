ac9d137c16c6bc7640642472f9ecb9d5e3070fef
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-06-03 02:03:58 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,8 @@
 set -xeuo
 clear
 
-mosdns-cn --service stop
-mosdns-cn --service uninstall
+/opt/mosdns-cn/bin/mosdns-cn --service stop
+/opt/mosdns-cn/bin/mosdns-cn --service uninstall
 
 rm -rf /opt/mosdns-cn
 sed -i '/mosdns-cn/d' /etc/crontab
