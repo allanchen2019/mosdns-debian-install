c2de7ce374b6bfd4c8cb78f889d938845a55cba9
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-06-03 02:02:05 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,14 +2,13 @@
 set -xeuo
 clear
 
+mosdns-cn --service stop
+mosdns-cn --service uninstall
+
 rm -rf /opt/mosdns-cn
 sed -i '/mosdns-cn/d' /etc/crontab
-
-systemctl stop mosdns-cn.service
-systemctl disable mosdns-cn.service
-rm /etc/systemd/system/mosdns-cn.service
+cd ~
 systemctl daemon-reload
-systemctl reset-failed
 
 rm -rf /etc/resolv.conf
 cat << EOF >/etc/resolv.conf
@@ -20,5 +19,4 @@ EOF
 systemctl enable systemd-resolved.service
 systemctl restart systemd-resolved.service
 
-cd ~
 echo "Uninstall complete."
