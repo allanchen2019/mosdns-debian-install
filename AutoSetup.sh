4f3740587d5271707f84366e962949f92e496cbf
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-06-19 02:50:31 +0800

    ·

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -10,5 +10,10 @@ git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns
 chmod 777 -R /opt/mosdns
 echo "执行安装……"
 bash /opt/mosdns/install-mosdns.sh
-echo "安装完成!"
+
+if systemctl status mosdns.service |grep -q "running"; then
+        echo "安装完成，mosdns已运行！"
+    else
+        echo "Emm...好像哪里不太对，mosdns没运行..."
+fi
 rm -rf ./AutoSetup.sh
