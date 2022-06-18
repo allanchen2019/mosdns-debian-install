0e867c79739a16b5c0ed1c98c1708411dc3cdbe8
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-06-18 23:35:59 +0800

    change to English echo

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -2,14 +2,14 @@
 set -xeuo pipefail
 clear
 
-echo "下载依赖……"
+echo "Downloading dependency……"
 apt update
 apt install wget git unzip pip -y
-echo "克隆存储库……"
+echo "Cloning repo……"
 git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns
 chmod 777 -R /opt/mosdns
-echo "运行安装脚本……"
+echo "Running……"
 bash /opt/mosdns/install-mosdns.sh
 #systemctl status mosdns.service
-echo "安装完成，运行journalctl -f |grep mosdns 观察dns查询状态。"
+echo "Finished! Run journalctl -f |grep mosdns to see what happend."
 rm -rf ./AutoSetup.sh
