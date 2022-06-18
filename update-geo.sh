b432dade797a09cc8b70299f0c6ff47af4385525
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2022-06-19 03:31:17 +0800

    ·

--- update-geo.sh
+++ update-geo.sh
@@ -4,15 +4,22 @@ set -euo pipefail
 clear
 cd /opt/mosdns/bin || exit
 
-wget -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
-wget -O apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
-wget -O anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
+wget --show-progress -t 5 -T 10 -cqO accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget --show-progress -t 5 -T 10 -cqO apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
+wget --show-progress -t 5 -T 10 -cqO anti-ad-domains.txt https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
 sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
 sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
 sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
 sed -r 's/.{16}$//' apple.china.conf2 > apple.china.conf.raw.txt
 python3 merge_cidr.py -s apnic -s ipip > chnroutes.txt 
 ./mosdns -s restart
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "……………资源文件更新完成……………"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+if systemctl status mosdns.service |grep -q "running"; then
+        echo "~~~~~~~~~~~~~~~~"
+        echo "资源文件更新完成！"
+        echo "~~~~~~~~~~~~~~~~"
+    else
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo "Emm………好像哪里不太对，mosdns挂了………"
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
