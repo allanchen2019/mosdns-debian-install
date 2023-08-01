4ecafe4f33d5a542fa773255bc04d533ca1eb679
allanchen2019 <50975936+allanchen2019@users.noreply.github.com>
2023-08-01 15:46:15 +0800

    display processes

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -5,10 +5,10 @@ echo "~~~~~~~~~~"
 echo "安装mosdns"
 echo "~~~~~~~~~~"
 echo "下载依赖……"
-apt update > /dev/null 2>&1
-apt install wget git unzip pip -y > /dev/null 2>&1
+apt update
+apt install wget git unzip pip -y
 echo "克隆库……"
-git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns > /dev/null 2>&1
+git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns
 chmod 777 -R /opt/mosdns
 echo "执行安装……"
 bash /opt/mosdns/install-mosdns.sh
